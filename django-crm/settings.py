2a755d1d2a84cbb7d32efff2e51b26763f65a7a4
xpxh <30749938+enriched-h@users.noreply.github.com>
2024-02-19 07:12:32 +0200

    changed db to sqlite

--- django-crm/settings.py
+++ django-crm/settings.py
@@ -63,11 +63,9 @@ WSGI_APPLICATION = 'dcrm.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "OPTIONS": {
-            "read_default_file": "/home/sihle/Desktop/Dev/Django-CRM/Django-CRM-main/my.cnf",
-        },
+     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
